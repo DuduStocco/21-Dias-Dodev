8f9f2f47120561a09d9901b09a244578fd077498
Dudu Stocco <dudu_stocco@hotmail.com>
2025-02-18 23:09:45 -0300

    aster

---
+++ Dia-3/index.js
@@ -0,0 +1,44 @@
+let tipotexto = "2"
+let tiponumber = 2
+let tipoBoolTexto = "True"
+let tipoBool = true
+
+// let e const
+let nome = "Carlos Stocco"
+console .log(nome)
+nome = "Eduardo Stocco"
+console .log(nome)
+
+//Use let quando precisar de uma variável que pode mudar durante a execução.//
+
+console.log("=====================Const==============")
+
+const nomeconst = "Carlos Stocco"
+console .log(nomeconst)
+nome = "Eduardo Stocco"
+console .log(nomeconst)
+
+//operadores aritméticos
+
+let numUm = 2
+let numDois = 4
+let numTres = numUm + numDois
+console.log(numTres) //6
+
+//Conversão de valores e entrada de dados
+
+let entradaDoUsuario = String (prompt("Insira um valor"))
+console.log(entradaDoUsuario) 
+let entradaDoUsuarioInt= Number (entradaDoUsuario)
+console.log(entradaDoUsuarioInt)
+
+//Exercicio
+
+let NomeDoUsuario = String (prompt ("Insira seu Nome"))
+console.log(NomeDoUsuario)
+let idadeDoUsuario = Number (prompt ("Insira sua idade"))
+console.log(idadeDoUsuario)
+let alturaDoUsuario = Number (prompt ("Insira sua altura"))
+console.log(alturaDoUsuario)
+let pesoDoUsuario = Number (prompt ("Insira seu peso"))
+console.log(pesoDoUsuario)
